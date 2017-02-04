435003f27e4aa4e569014da37f9dacda51b6baf6
bobby569 <zhan2175@purdue.edu>
2017-02-03 22:00:22 -0500

    update

--- Assignment1/globitek/public/register.php
+++ Assignment1/globitek/public/register.php
@@ -8,7 +8,8 @@
   // Hint: private/functions.php can help
 
   // Confirm that POST values are present before accessing them.
-  if (isset($_POST['submit'])) {
+  //if (isset($_POST['submit'])) {
+  if (is_post_request()) {
     $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
     $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
@@ -17,32 +18,32 @@
     // Perform Validations
     // Hint: Write these in private/validation_functions.php
     if (is_blank($firstname)) {
-      array_push($errors, "First name cannot be blank.");
+      array_push($errors, "First name cannot be blank");
     } elseif (!has_length($firstname, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "First name must be between 2 and 20 characters.");
+      array_push($errors, "First name must be between 2 and 20 characters");
     }
     if (is_blank($lastname)) {
-      array_push($errors, "Last name cannot be blank.");
+      array_push($errors, "Last name cannot be blank");
     } elseif (!has_length($lastname, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "Last name must be between 2 and 20 characters.");
+      array_push($errors, "Last name must be between 2 and 20 characters");
     }
     if (is_blank($email)) {
-      array_push($errors, "Email cannot be blank.");
+      array_push($errors, "Email cannot be blank");
     } elseif (!has_valid_email_format($email)) {
-      array_push($errors, "The email format does not seem right.");
+      array_push($errors, "The email format does not seem right");
     }
     if (is_blank($username)) {
-      array_push($errors, "First name cannot be blank.");
+      array_push($errors, "First name cannot be blank");
     } elseif (!has_length($username, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "First name must be between 2 and 20 characters.");
-    } elseif (starts_with_alpha($username)) {
-      array_push($errors, "The username needs to start with alphabet.");
+      array_push($errors, "First name must be between 2 and 20 characters");
+    } elseif (!starts_with_alpha($username)) {
+      array_push($errors, "The username needs to start with alphabet");
     }
     if (is_blank($password)) {
       array_push($errors, "The password cannot be empty");
     }
     if (!empty($errors)) {
-      globals $hidden = "";
+      $hidden = "";
     } else {
       // if there were no errors, submit data to database
       $sql = "";
@@ -50,6 +51,8 @@
       if ($result) {
         db_close($db);
         // TODO redirect user to success page
+//        header("Location: ./registration_success.php");
+//        exit;
       } else {
         echo db_error($db);
         db_close($db);
@@ -72,23 +75,23 @@
     <ul>
     <?php
       foreach ($errors as $err) {
-        echo "<li>$err</li>";
+        echo "<li class='error'>$err</li>";
       }
      ?>
-   </ul>
+    </ul>
   </div>
 
   <form id="form" action="register.php" method="post" style="font-family: cursive">
     First name:<br>
-    <input type="text" name="firstname"><br>
+    <input class="input_box" type="text" name="firstname" value="<?php echo $_POST['firstname']; ?>"><br>
     Last name:<br>
-    <input type="text" name="lastname"><br>
+    <input class="input_box" type="text" name="lastname" value="<?php echo $_POST['lastname']; ?>"><br>
     Email: <br>
-    <input type="text" name="email"><br>
+    <input class="input_box" type="text" name="email" value="<?php echo $_POST['email']; ?>"><br>
     Username: <br>
-    <input type="text" name="username"><br>
+    <input class="input_box" type="text" name="username" value="<?php echo $_POST['username']; ?>"><br>
     Password: <br>
-    <input type="password" name="password"><br><br>
+    <input class="input_box" type="password" name="password"><br><br>
     <input type="submit" name="submit" value="Submit">
   </form>
 
