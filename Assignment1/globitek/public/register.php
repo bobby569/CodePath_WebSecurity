8be7de8d66961df0b4020d1e397cba3202864f86
bobby569 <zhan2175@purdue.edu>
2017-02-03 23:11:48 -0500

    update

--- Assignment1/globitek/public/register.php
+++ Assignment1/globitek/public/register.php
@@ -5,48 +5,46 @@
   $hidden = "hidden";
   $errors = array();
   // if this is a POST request, process the form
-  // Hint: private/functions.php can help
-
-  // Confirm that POST values are present before accessing them.
   //if (isset($_POST['submit'])) {
   if (is_post_request()) {
+    // Retrieve data
     $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
     $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $username = isset($_POST['username']) ? $_POST['username'] : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
     // Perform Validations
-    // Hint: Write these in private/validation_functions.php
     if (is_blank($firstname)) {
       array_push($errors, "First name cannot be blank");
-    } elseif (!has_length($firstname, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "First name must be between 2 and 20 characters");
+    } elseif (!has_length($firstname, ['min' => 2, 'max' => 255])) {
+      array_push($errors, "First name must be between 2 and 255 characters");
     }
     if (is_blank($lastname)) {
       array_push($errors, "Last name cannot be blank");
-    } elseif (!has_length($lastname, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "Last name must be between 2 and 20 characters");
+    } elseif (!has_length($lastname, ['min' => 2, 'max' => 255])) {
+      array_push($errors, "Last name must be between 2 and 255 characters");
     }
     if (is_blank($email)) {
       array_push($errors, "Email cannot be blank");
+    } elseif (!has_length($email, ['max' => 255])) {
+        array_push($errors, "Email must be less than 255 characters");
     } elseif (!has_valid_email_format($email)) {
-      array_push($errors, "The email format does not seem right");
+      array_push($errors, "The email format is invalid");
     }
     if (is_blank($username)) {
-      array_push($errors, "First name cannot be blank");
-    } elseif (!has_length($username, ['min' => 2, 'max' => 20])) {
-      array_push($errors, "First name must be between 2 and 20 characters");
+      array_push($errors, "Username cannot be blank");
+    } elseif (!has_length($username, ['min' => 8, 'max' => 255])) {
+      array_push($errors, "First name must be between 2 and 255 characters");
     } elseif (!starts_with_alpha($username)) {
       array_push($errors, "The username needs to start with alphabet");
     }
-    if (is_blank($password)) {
-      array_push($errors, "The password cannot be empty");
-    }
+    // React to the validations
     if (!empty($errors)) {
       $hidden = "";
     } else {
       // if there were no errors, submit data to database
-      $sql = "";
+      $date = date("Y-m-d H:i:s");
+      $sql = "INSERT INTO globitek.USERS (FIRSTNAME, LASTNAME, EMAIL, USERNAME, CREATE_AT) ";
+      $sql .= "VALUES ('$firstname', '$lastname', '$email', '$username', '$date')";
       $result = db_query($db, $sql);
       if ($result) {
         db_close($db);
@@ -81,19 +79,17 @@
     </ul>
   </div>
 
-  <form id="form" action="register.php" method="post" style="font-family: cursive">
-    First name:<br>
-    <input class="input_box" type="text" name="firstname" value="<?php echo $_POST['firstname']; ?>"><br>
-    Last name:<br>
-    <input class="input_box" type="text" name="lastname" value="<?php echo $_POST['lastname']; ?>"><br>
-    Email: <br>
-    <input class="input_box" type="text" name="email" value="<?php echo $_POST['email']; ?>"><br>
-    Username: <br>
-    <input class="input_box" type="text" name="username" value="<?php echo $_POST['username']; ?>"><br>
-    Password: <br>
-    <input class="input_box" type="password" name="password"><br><br>
-    <input type="submit" name="submit" value="Submit">
-  </form>
+    <form id="form" action="register.php" method="post">
+        First name:<br>
+        <input class="input_box" type="text" name="firstname" value="<?php echo $firstname; ?>"><br>
+        Last name:<br>
+        <input class="input_box" type="text" name="lastname" value="<?php echo $lastname; ?>"><br>
+        Email: <br>
+        <input class="input_box" type="text" name="email" value="<?php echo $email; ?>"><br>
+        Username: <br>
+        <input class="input_box" type="text" name="username" value="<?php echo $username; ?>"><br><br>
+        <input type="submit" name="submit" value="Submit" style="margin-bottom: 5px">
+    </form>
 
 </div>
 
