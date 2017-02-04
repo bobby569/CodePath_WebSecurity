f1af76700af2a2be1678e1cf73e8afeb5f0ad57e
bobby569 <zhan2175@purdue.edu>
2017-02-03 21:05:33 -0500

    update

--- Assignment1/globitek/public/register.php
+++ Assignment1/globitek/public/register.php
@@ -13,32 +13,36 @@
     $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
     // Perform Validations
     // Hint: Write these in private/validation_functions.php
     if (is_blank($firstname)) {
-      $errors["err1"] = "First name cannot be blank.";
+      array_push($errors, "First name cannot be blank.");
     } elseif (!has_length($firstname, ['min' => 2, 'max' => 20])) {
-      $errors["err1"] = "First name must be between 2 and 20 characters.";
+      array_push($errors, "First name must be between 2 and 20 characters.");
     }
     if (is_blank($lastname)) {
-      $errors["err2"] = "Last name cannot be blank.";
+      array_push($errors, "Last name cannot be blank.");
     } elseif (!has_length($lastname, ['min' => 2, 'max' => 20])) {
-      $errors["err2"] = "Last name must be between 2 and 20 characters.";
+      array_push($errors, "Last name must be between 2 and 20 characters.");
     }
     if (is_blank($email)) {
-      $errors["err3"] = "Email cannot be blank.";
+      array_push($errors, "Email cannot be blank.");
     } elseif (!has_valid_email_format($email)) {
-      $errors["err3"] = "The email format does not seem right.";
+      array_push($errors, "The email format does not seem right.");
     }
     if (is_blank($username)) {
-      $errors["err4"] = "First name cannot be blank.";
+      array_push($errors, "First name cannot be blank.");
     } elseif (!has_length($username, ['min' => 2, 'max' => 20])) {
-      $errors["err4"] = "First name must be between 2 and 20 characters.";
+      array_push($errors, "First name must be between 2 and 20 characters.");
     } elseif (starts_with_alpha($username)) {
-      $errors["err4"] = "The username needs to start with alphabet.";
+      array_push($errors, "The username needs to start with alphabet.");
     }
-    if (!empty($error)) {
-      $hidden = "";
+    if (is_blank($password)) {
+      array_push($errors, "The password cannot be empty");
+    }
+    if (!empty($errors)) {
+      globals $hidden = "";
     } else {
       // if there were no errors, submit data to database
       $sql = "";
@@ -64,12 +68,11 @@
   <p>Register to become a Globitek Partner.</p>
 
   <div <?php echo $hidden; ?>>
+    <p>Please fix the following error(s): </p>
     <ul>
     <?php
-      foreach ($errors as $key => $value) {
-        if (isset($key)) {
-          echo "<li>$value</li>";
-        }
+      foreach ($errors as $err) {
+        echo "<li>$err</li>";
       }
      ?>
    </ul>
