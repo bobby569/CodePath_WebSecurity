46a11e92219717db484f946f005f5247920b3fa8
bobby569 <zhan2175@purdue.edu>
2017-02-27 19:26:22 -0500

    obj1&2 done

--- Assignment5/apex/private/crypto_functions.php
+++ Assignment5/apex/private/crypto_functions.php
@@ -6,13 +6,29 @@
 const CIPHER_METHOD = 'AES-256-CBC';
 
 function key_encrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "D4RK SH4D0W RUL3Z";
+  $key = str_pad($key, 32, '*');
+  $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(CIPHER_METHOD));
+  // Encrypt
+  $encrypted = openssl_encrypt($string, CIPHER_METHOD, $key, OPENSSL_RAW_DATA, $iv);
+  // Return $iv at front of string, need it for decoding
+  $message = $iv . $encrypted;
+  // Encode just ensures encrypted characters are viewable/savable
+  return base64_encode($message);
 }
 
 function key_decrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "PWNED YOU!";
+  // Needs a key of length 32 (256-bit)
+  $key = str_pad($key, 32, '*');
+  // Base64 decode before decrypting
+  $iv_with_ciphertext = base64_decode($string);
+  // Separate initialization vector and encrypted string
+  $iv_length = openssl_cipher_iv_length(CIPHER_METHOD);
+  $iv = substr($iv_with_ciphertext, 0, $iv_length);
+  $ciphertext = substr($iv_with_ciphertext, $iv_length);
+  // Decrypt
+  $plaintext = openssl_decrypt($ciphertext, CIPHER_METHOD, $key, OPENSSL_RAW_DATA, $iv);
+  return $plaintext;
 }
-
 
 // Asymmetric Encryption / Public-Key Cryptography
 
@@ -31,11 +47,11 @@ function generate_keys($config=PUBLIC_KEY_CONFIG) {
 }
 
 function pkey_encrypt($string, $public_key) {
-  return 'Qnex Funqbj jvyy or jngpuvat lbh';
+  //return key_encrypt($string, $public_key);
 }
 
 function pkey_decrypt($string, $private_key) {
-  return 'Alc evi csy pssomrk livi alir csy wlsyph fi wezmrk ETIB?';
+  return str_rot13($string);
 }
 
 
